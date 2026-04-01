fc484ce01207b31c51389cda11817bb4e05579d3
hyori100 <ag497752@naver.com>
2026-04-01 23:55:42 +0900

    답변 기능 만들기

---
+++ src/main/java/com/example/sbb/answer/AnswerController.java
@@ -0,0 +1,28 @@
+package com.example.sbb.answer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.sbb.question.Question;
+import com.example.sbb.question.QuestionService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+	private final QuestionService questionService;
+	private final AnswerService answerSerivce;
+	
+	@PostMapping("/create/{id}")
+	public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+		Question question = this.questionService.getQuestion(id);
+		this.answerSerivce.create(question, content);
+		return String.format("redirect:/question/detail/%s", id);
+	}
+}
